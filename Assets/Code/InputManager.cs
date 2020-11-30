b7fe97d808787bb9ed8f99372eb4d322ba6c6ab8
Nina Mäkinen <sherlocked1223@gmail.com>
2020-11-30 13:49:48 +0200

    Project start

---
+++ Assets/Code/InputManager.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    //Make sure there is only one input manager
+    private static InputManager _instance;
+    public static InputManager Instance
+    {
+        get
+        {
+            return _instance;
+        }
+    }
+    
+    private PlayerControls playerControls;
+    
+    //Get this befote start
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+        playerControls = new PlayerControls();
+    }
+
+    //Enable Controls
+    private void OnEnable()
+    {
+        playerControls.Enable();
+    }
+    //Disable controls
+    private void OnDisable()
+    {
+        playerControls.Disable();
+    }
+    //Get movement
+    public Vector2 GetPlayerMovement()
+    {
+        return playerControls.Player.Movement.ReadValue<Vector2>();
+    }
+
+    //Get mouse movement
+    public Vector2 GetMouseDelta()
+    {
+        return playerControls.Player.Look.ReadValue<Vector2>();
+    }
+
+    public bool PlayerJumped()
+    {
+        return playerControls.Player.Jump.triggered;
+    }
+}
---
+++ Assets/Code/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+
+//This won't run if character controller is not set
+[RequireComponent(typeof(CharacterController))]
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    private float playerSpeed = 2.0f;
+    [SerializeField]
+    private float jumpHeight = 1.0f;
+    [SerializeField]
+    private float gravityValue = -9.81f;
+    [SerializeField]
+    private float rotationSpeed = 4f;
+    private InputManager inputManager;
+    private CharacterController controller;
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    public Transform mainCameraTransform;
+
+    private void Start()
+    {
+        controller = GetComponent<CharacterController>();
+        inputManager = InputManager.Instance;
+    }
+
+    void Update()
+    {
+        //If character is grounded
+        groundedPlayer = controller.isGrounded;
+        if (groundedPlayer && playerVelocity.y < 0)
+        {
+            playerVelocity.y = 0f;
+        }
+        Vector2 movement = inputManager.GetPlayerMovement();
+        Vector3 move = new Vector3(movement.x, 0, movement.y);
+        move = mainCameraTransform.forward * move.z + mainCameraTransform.right * move.x;
+        move.y = 0f;
+        controller.Move(move * Time.deltaTime * playerSpeed);
+
+        // Changes the height position of the player..
+        if (inputManager.PlayerJumped() && groundedPlayer)
+        {
+            playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+        }
+
+        playerVelocity.y += gravityValue * Time.deltaTime;
+        controller.Move(playerVelocity * Time.deltaTime);
+        if(movement != Vector2.zero)
+        {
+            float targetAngle = Mathf.Atan2(movement.x, movement.y) * Mathf.Deg2Rad + mainCameraTransform.eulerAngles.y;
+            Quaternion rotation = Quaternion.Euler(0f, targetAngle, 0f);
+            transform.rotation = Quaternion.Lerp(transform.rotation, rotation, Time.deltaTime * rotationSpeed);
+        }
+    }
+}
