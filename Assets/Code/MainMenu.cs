4149be831d84808b4760b1c59be0b3dccdc84331
Nina Mäkinen <sherlocked1223@gmail.com>
2020-12-30 18:34:28 +0200

    TMP added and first version of main menu.

---
+++ Assets/Code/MainMenu.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MainMenu : MonoBehaviour
+{
+    // Play game
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    //Quit game
+    public void QuitGame()
+    {
+       Debug.Log("Quitting..."); 
+       Application.Quit();
+    }
+       
+    
+}
